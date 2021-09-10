03655e2d10ab6d5b3bd1482cec45d296d320167d
Fatima <fcamero12@gmail.com>
2021-09-10 13:44:48 -0700

    Create test to check if url and likes content are shown when View button is clicked

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
 
@@ -27,4 +27,37 @@ test('<Blog /> displays title and author but does not render url or likes by def
   expect(component.container).toHaveTextContent('blog@example.com')
   expect(component.container).toHaveTextContent('likes: 10')
   expect(toggleableDiv).toHaveStyle('display: none')
+})
+
+test('test toggled content can be viewed', () => {
+  // const handleViewClick = jest.fn()
+
+  const blog = {
+    title: 'This title is always displayed',
+    author: 'L Kent',
+    url: 'blog@example.com',
+    likes: 10,
+    user: {
+      name: 'testUser'
+    }
+  }
+
+  const userName = 'testUser'
+
+  const component = render(
+    <Blog blog={blog} userName={userName}/>
+  )
+
+  component.debug()
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+
+  const toggleableDiv = component.container.querySelector('.toggleableDiv')
+
+  expect(component.container).toHaveTextContent('This title is always displayed')
+  expect(component.container).toHaveTextContent('L Kent')
+  expect(component.container).toHaveTextContent('blog@example.com')
+  expect(component.container).toHaveTextContent('likes: 10')
+  expect(toggleableDiv).not.toHaveStyle('display: none')
 })
